b6599fe6f8eed8f1e4d98376bf883cac0899d663
洪坤越 <hongkunyue.123@gmail.com>
2020-09-02 15:15:25 +0800

    第二次更改~~

--- vue.config.js
+++ vue.config.js
@@ -13,7 +13,7 @@ const buildMsg = 'build at ' + new Date()
 
 const resolve = dir => {
     return path.join(__dirname, dir)
-} //
+} //第二次更改
 // 这里需要修改为你线上项目存放的路径
 // 比如你打包的文件放到服务器的my-app文件夹，域名为a.com，则应改为
 // http(s)://a.com/my-app/
